0bf8f32c5e81df1cd290cf1b3fad1651ff41930e
Banana-programadora <damofeasta@gmail.com>
2024-08-11 12:52:21 +0200

    Add files via upload

---
+++ insertar_codigos.js
@@ -0,0 +1,51 @@
+var codigo = prompt("Inserta un codigo")
+
+switch(codigo){
+    case 'patata':
+        document.write("A mi también me gustan las patatas.")
+        break;
+    case 'webiweb':
+        document.write("Tienes razón, webiweb es la mejor web.")
+        break;
+    case 'python':
+        document.write("XD esto está hecho con javascript")
+        break;
+    case 'sheldon':
+        document.write("Oh no, me has bazingueado")
+        break;
+    case '¿esto va en serio?':
+        document.write("Si, va en serio")
+        break;
+    case 'caca':
+        document.write("Debes de tener como 6 añitos")
+        break;
+    case 'perro salchicha':
+        document.write("gordo bachicha")
+        break;
+    case 'Homosexual':
+        document.write("Eso diselo al femboy")
+        break;
+    case 'illojuan':
+        document.write("No me juzgues por ser Illojuaner")
+        break;
+    case 'noob':
+        document.write("Oh, :(")
+        break;
+    case 'chatgpt':
+        document.write("No se usa eso aquí")
+        break;
+    case '3':
+        document.write("Eres un verdadero Webiweber")
+        break;
+    case 'bill cipher':
+        alert("SABES DEMASIADO, IREMOS A POR TI (es no-canon)")
+        break;
+    case '08/08/2024':
+        document.write("Eh, tu saves nucho vien.")
+        break;
+    case 'nvv':
+        document.write("Esto se te da bien. Tomaun webipunto.")
+        break;
+    default:
+        document.write("Ese código no existe.")
+}
